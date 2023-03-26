37bb4bcd2985b03184b484803cbb1dc32bfa92d8
Alexander Valenzuela <axvalenzuela@gmail.com>
2023-03-26 16:16:16 -0700

    update 03_26_2023

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -28,3 +28,29 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+
+"""Writes the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path provided at prompt from the save_qualifying_loans function in app.py.
+
+    Returns:
+        A list of lists that contains the rows of data from the save_qualifying_loans function in app.py.
+
+    """
+
+def save_csv(csvpath,data):
+    header = ["Lender","Max Loan Amount","Max LTV","Max DTI","Min Credit Score","Interest Rate"]
+    #Opens file in write mode.
+    with open(csvpath, 'w', newline='') as csvfile:
+        #Writes data to file.
+        csvwriter = csv.writer(csvfile, delimiter=",")
+        #Write the header
+        csvwriter.writerow(header)
+        #Write the data rows
+        for row in data:
+            csvwriter.writerow(row)
+
+    
+        
